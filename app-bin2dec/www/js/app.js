1a9918f3eeece785de7e676e044d8f3d58946863
Suellen <73621600+suellen05@users.noreply.github.com>
2022-07-19 20:12:18 -0300

    APP

--- app-bin2dec/www/js/app.js
+++ app-bin2dec/www/js/app.js
@@ -12,23 +12,23 @@ document.querySelector('#btn-calcular').addEventListener('click', () => {
 })
 
 const varejo = () => {
-    let valor = produto.value * 0.5   
+    let valor = produto.value * 0.3   
     document.querySelector('#varejo').innerHTML = `<p class="text-center">Peças = ${valor}</p>`
     
 }
 
 const tivoli = () => {
-    let valor = produto.value * 0.5   
+    let valor = produto.value * 0.15   
     document.querySelector('#tivoli').innerHTML = `<p class="text-center">Peças = ${valor}</p>`
 }
 
 const rioclaro = () => {
-    let valor = produto.value * 0.5   
+    let valor = produto.value * 0.12   
     document.querySelector('#rioclaro').innerHTML = `<p class="text-center">Peças = ${valor}</p>`
 }
 
 const limeira = () => {
-    let valor = produto.value * 0.5   
+    let valor = produto.value * 0.10 
     document.querySelector('#limeira').innerHTML = `<p class="text-center">Peças = ${valor}</p>`
 }
 
